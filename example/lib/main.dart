77489e9320beab2af8a92bd36539c83a4de8e781
FloodCode <amosogra@gmail.com>
2021-03-26 17:13:07 +0100

    Update main.dart

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
-import 'dart:async';
 
-import 'package:flutter/services.dart';
 import 'package:flutter_pip/flutter_pip.dart';
-import 'package:flutter_pip/models/pip_ratio.dart';
 import 'package:flutter_pip/platform_channel/channel.dart';
 import 'package:video_player/video_player.dart';
 
